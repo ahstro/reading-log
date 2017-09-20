99c3dec0a08d0606854945cd2cdc757bde00e678
Anton Strömkvist <anton@stromkvist.com>
2017-09-20 09:03:14 +0200

    Add ISBN to books

--- app/Model.elm
+++ app/Model.elm
@@ -6,6 +6,10 @@ module Model
         )
 
 
+type ISBN
+    = ISBN Int
+
+
 type Page
     = Page Int
 
@@ -14,6 +18,7 @@ type alias Book =
     { name : String
     , author : String
     , pageCount : ( Page, Page )
+    , isbn : ISBN
     }
 
 
@@ -31,6 +36,7 @@ init =
                 ( Page 8
                 , Page 205
                 )
+            , ISBN 9789173894944
             ]
       }
     , Cmd.none

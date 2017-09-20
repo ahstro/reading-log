0e02cf71c5f0f5b4767e3e136310722abeb1ecc3
Anton Strömkvist <anton@stromkvist.com>
2017-09-20 08:35:11 +0200

    Miiiinor styling and add classes for future styling

--- app/View.elm
+++ app/View.elm
@@ -1,18 +1,21 @@
 module View exposing (view)
 
 import Html exposing (Html, div, text)
+import Html.Attributes exposing (class)
 import Model exposing (Model)
 import Update exposing (Msg(..))
 
 
 view : Model -> Html Msg
 view model =
-    div [] <|
-        List.map
-            (\book ->
-                div []
-                    [ text book.name
-                    , text book.author
-                    ]
-            )
-            model.books
+    div []
+        [ div [ class "books" ] <|
+            List.map
+                (\book ->
+                    div [ class "book" ]
+                        [ div [] [ text book.name ]
+                        , div [] [ text book.author ]
+                        ]
+                )
+                model.books
+        ]
